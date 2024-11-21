450cced50dcc4ded6bef34f9a787c5e7a9fa1229
Mikkel Ricky <rimi@aarhus.dk>
2024-11-21 10:45:31 +0100

    Converted script to plain sh

--- data-export/export.sh
+++ data-export/export.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
-set -o errexit -o errtrace -o noclobber -o nounset -o pipefail
-IFS=$'\n\t'
+#!/usr/bin/env sh
 
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+script_dir=$(cd "$(dirname "$0")" && pwd)
 
-function usage() {
+usage() {
     if [ -n "${1:-}" ]; then
     >&2 cat <<EOF
 $1
@@ -13,40 +11,36 @@ EOF
     fi
 
     >&2 cat <<EOF
-Usage: ${BASH_SOURCE[0]} site-uri
+Usage: $0 site-uri
 
 EOF
     exit 1
 }
 
-if (( $# < 1 )); then
-    usage "Too few arguments"
-fi
-
 site_uri="$1"
 
 if [ -z "$site_uri" ]; then
-    usage "Invalid site-uri"
+    usage "Missing site-uri"
 fi
 
 # cd "$project_dir"
 
-filenames=("$script_dir"/export_*.sql)
+filenames="$script_dir/export_*.sql"
 
-for filename in "${filenames[@]}"; do
+for filename in $filenames; do
     echo "Running $filename"
 
     # JSON
 
     # https://tldp.org/LDP/abs/html/string-manipulation.html
-    output_filename=${filename/%.sql/.json}
+    output_filename="$filename.json"
     # https://github.com/drush-ops/drush/issues/3071#issuecomment-347929777
     vendor/bin/drush --uri="$site_uri" php:eval "return \Drupal::database()->query(file_get_contents('$filename'))->fetchAll()" --format=json >| "$output_filename" || true
     ls -lh "$output_filename"
 
     # CSV
 
-    output_filename=${filename/%.sql/.csv}
+    output_filename="$filename.csv"
     # https://stackoverflow.com/a/22421445/2502647
     vendor/bin/drush --uri="$site_uri" sql:cli < "$filename" | awk 'BEGIN { FS="\t"; OFS="," } {
   rebuilt=0
