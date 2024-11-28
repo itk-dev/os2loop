525bb143481cebdeb50aa41c7a0f24cad37b3416
Daniel Andreasen <akda@aarhus.dk>
2024-11-28 07:58:32 +0100

    Add status true condition
    Also adds some comments, since the queries are copied from itk-ai/loop_vectorstore develop branch

--- data-export/export_external_with_meta.sql
+++ data-export/export_external_with_meta.sql
@@ -1,4 +1,4 @@
-Select
+Select 
   n_fd.nid,
   n_fd.type,
   n_fd.title,
@@ -15,27 +15,28 @@ Select
   tags.tags,
   rev_date.os2loop_shared_rev_date_value as review_date
 from (
-  SELECT nid,vid,type,uid,title,created,changed
+  SELECT nid,vid,type,uid,title,created,changed 
   FROM node_field_data
   where type = 'os2loop_external'  -- nodes with links/external references (it seems)
+    and status = 1
   ) as n_fd
 left join path_alias as pa on CONCAT('/node/',n_fd.nid) = pa.path
 left join user__os2loop_user_job_title as u_jt on n_fd.uid = u_jt.entity_id
 left join node__os2loop_external_descripti as ext_desc on n_fd.nid = ext_desc.entity_id
 left join node__os2loop_external_url as ext_url on n_fd.nid = ext_url.entity_id
 left join (
-  SELECT
-    n_ss.entity_id,
-    subject_tt_fd.name
-  FROM node__os2loop_shared_subject as n_ss
-  left join taxonomy_term_field_data as subject_tt_fd on n_ss.os2loop_shared_subject_target_id = subject_tt_fd.tid
-  where n_ss.bundle = 'os2loop_external') as `subject` on n_fd.nid = `subject`.entity_id
+	SELECT 
+		n_ss.entity_id,
+		subject_tt_fd.name 
+	FROM node__os2loop_shared_subject as n_ss
+	left join taxonomy_term_field_data as subject_tt_fd on n_ss.os2loop_shared_subject_target_id = subject_tt_fd.tid
+	where n_ss.bundle = 'os2loop_external') as `subject` on n_fd.nid = `subject`.entity_id
 left join (
-  SELECT
-    n_st.entity_id,
-        json_arrayagg(tt_fd.name) as tags
-  FROM node__os2loop_shared_tags  as n_st
-  left join taxonomy_term_field_data as tt_fd on n_st.os2loop_shared_tags_target_id = tt_fd.tid
-  where n_st.bundle = 'os2loop_external'
-  group by  n_st.entity_id) tags on n_fd.nid = tags.entity_id
+	SELECT 
+		n_st.entity_id, 
+        json_arrayagg(tt_fd.name) as tags 
+	FROM node__os2loop_shared_tags  as n_st
+	left join taxonomy_term_field_data as tt_fd on n_st.os2loop_shared_tags_target_id = tt_fd.tid
+	where n_st.bundle = 'os2loop_external' 
+	group by  n_st.entity_id) tags on n_fd.nid = tags.entity_id
 left join node__os2loop_shared_rev_date as rev_date on n_fd.nid = rev_date.entity_id
